1082b1293932d2fbc23173e98138b632ceba464e
NestorSanchez <nesaenz.69@gmail.com>
2023-01-31 17:04:46 -0500

    add upload ans download nodemon

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const { API_VERSION } = require("./constans");
 
 const app = express();
@@ -6,8 +8,14 @@ const app = express();
 //import routings
 
 //_configure Body Parse
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Configure static folder
+app.use(express.static("uploads"));
 
 //Configure Header HTTP -  CORS
+app.use(cors());
 
 //Configure routings
 
