4ea9165ae4760aedda4563f2ee1f13ab7a2b1e27
NestorSanchez <nesaenz.69@gmail.com>
2023-02-03 17:48:10 -0500

    create jsonwentoken

---
+++ utils/jwt.js
@@ -0,0 +1,39 @@
+const jwt = requiere("jsonwebtoken");
+const { JWT_SECRET_KEY } = require("../constans");
+
+function createAccessToken(user) {
+  const expToken = new Date();
+  expToken.setHours(expToken.getHours() + 3);
+
+  const payload = {
+    token_type: "access",
+    user_id: user._id,
+    iat: Date.now(),
+    exp: expToken.getTime(),
+  };
+
+  return jwt.sign(payload, JWT_SECRET_KEY);
+}
+
+function createRefreshToken(user) {
+  expToken.getMonth(expToken.getMonth() + 1);
+
+  const payload = {
+    token_type: "refresh",
+    user_id: user._id,
+    iat: Date.now(),
+    exp: expToken.getTime(),
+  };
+
+  return jwt.sign(payload, JWT_SECRET_KEY);
+}
+
+function decoded(token) {
+  return jwt.decoded(token, JWT_SECRET_KEY);
+}
+
+module.exports = {
+  createAccessToken,
+  createRefreshToken,
+  decoded,
+};
