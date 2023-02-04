415f0824ddfaece35e21cd77061c5a6187377fb7
NestorSanchez <nesaenz.69@gmail.com>
2023-02-03 22:59:30 -0500

    function login sucessfull

--- constans.js
+++ constans.js
@@ -5,6 +5,8 @@ const DB_HOST = "secondpageintalnet.ld9gasl.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "hjdisuhiouay89f897f89adshfu";
+
 module.exports = {
   DB_USER,
   DB_PASSWORD,
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 function register(req, res) {
   const { firstName, lastName, email, password } = req.body;
@@ -27,6 +28,33 @@ function register(req, res) {
   });
 }
 
+function login(req, res) {
+  const { email, password } = req.body;
+  if (!email) res.status(401).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(401).send({ msg: "La contraseña  es obligatorio" });
+
+  const emailLowercase = email.toLowerCase();
+
+  User.findOne({ email: emailLowercase }, (error, userStore) => {
+    if (error) {
+      res.status(404).send({ msg: "Error del servidor" });
+    } else {
+      bcrypt.compare(password, userStore.password, (bcryptError, check) => {
+        if (bcryptError) {
+          res.status(500).send({ msg: "Error del servidor" });
+        } else if (!check) {
+          res.status(500).send({ msg: "Contraseña incorrecta" });
+        } else if (!userStore.active) {
+          res.status(401).send({ msg: "Usuario no autorizado o no activo" });
+        } else {
+          res.status(200).send({ result: "OK" });
+        }
+      });
+    }
+  });
+}
+
 module.exports = {
   register,
+  login,
 };
