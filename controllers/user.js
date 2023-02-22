9f2409713c4d191dead6a0165c2da7125a97163f
NestorSanchez <nesaenz.69@gmail.com>
2023-02-22 15:18:18 -0500

    crate file image.js for process image

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 async function getMe(req, res) {
   const { user_id } = req.user;
@@ -25,8 +26,22 @@ async function getUsers(req, res) {
 }
 
 async function createUser(req, res) {
-  console.log(req.body);
-  res.status(200).send({ msg: "OK" });
+  const { password } = req.body;
+
+  const salt = bcrypt.genSaltSync(10);
+  const hasPassword = bcrypt.hashSync(password, salt);
+  const user = new User({ ...req.body, actuve: false, password: hasPassword });
+
+  if (req.files.avatar) {
+    console.log("Procesar avatar");
+  }
+  user.save((error, userStore) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear usuario" });
+    } else {
+      res.status(201).send(userStore);
+    }
+  });
 }
 
 module.exports = {
