230972577bae351fc67255393abddb54e31b0123
NestorSanchez <nesaenz.69@gmail.com>
2023-02-22 22:27:40 -0500

    creta function deleteUser, fuction successfull

--- controllers/user.js
+++ controllers/user.js
@@ -50,10 +50,47 @@ async function createUser(req, res) {
 async function updateUser(req, res) {
   const { id } = req.params;
   const userData = req.body;
+
+  //vulve a encriptar la contraseña cuando se actualiza
+  if (userData.password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(userData.password, salt);
+    userData.password = hashPassword;
+  } else {
+    delete userData.password;
+  }
+
+  //Para actualizacion de imagen avatar
+  if (req.files.avatar) {
+    const imagePath = image.getFilePath(req.files.avatar);
+    userData.avatar = imagePath;
+  }
+
+  User.findByIdAndUpdate({ _id: id }, userData, (error) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al actualizar usuario" });
+    } else {
+      res.status(200).send({ msg: "Actualización correcta" });
+    }
+  });
+}
+
+async function deleteUser(req, res) {
+  const { id } = req.params;
+
+  User.findOneAndDelete(id, (error) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al eliminar usuario" });
+    } else {
+      res.status(200).send({ msg: "Usuario eliminado satisfactoriamente" });
+    }
+  });
 }
 
 module.exports = {
   getMe,
   getUsers,
   createUser,
+  updateUser,
+  deleteUser,
 };
