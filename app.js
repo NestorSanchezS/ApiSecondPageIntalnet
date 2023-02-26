3a3aa9af439451e1c18c40d8cfebccda815f30ff
NestorSanchez <nesaenz.69@gmail.com>
2023-02-25 22:20:22 -0500

    add function createMenu succesfull

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const app = express();
 //import routings
 const authRoutes = require("./router/auth");
 const userRoutes = require("./router/user");
+const menuRoutes = require("./router/menu");
 
 //_configure Body Parse
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Configure static folder
 app.use(express.static("uploads"));
@@ -22,5 +23,6 @@ app.use(cors());
 //Configure routings
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, menuRoutes);
 
 module.exports = app;
