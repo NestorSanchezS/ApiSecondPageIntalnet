596e575b5750dbaa1e584ddba7ba05a3554ad776
NestorSanchez <nesaenz.69@gmail.com>
2023-02-25 22:39:51 -0500

    add function getMenus, fuction successfull

--- controllers/menu.js
+++ controllers/menu.js
@@ -11,6 +11,24 @@ async function createMenu(req, res) {
   });
 }
 
+async function getMenus(req, res) {
+  const { active } = req.query;
+
+  let response = null;
+
+  if (active === undefined) {
+    response = await Menu.find().sort({ order: "asc" });
+  } else {
+    response = await Menu.find({ active }).sort({ order: "asc" });
+  }
+  if (!response) {
+    res.status(400).send({ msg: "No se ha encontado ningun menú" });
+  } else {
+    res.status(200).send(response);
+  }
+}
+
 module.exports = {
   createMenu,
+  getMenus,
 };
