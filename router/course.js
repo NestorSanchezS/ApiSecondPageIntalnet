e96e9b3c9afa60ca5e5918d6f595fbc1144d9b4c
NestorSanchez <nesaenz.69@gmail.com>
2023-02-27 16:26:35 -0500

    create filder course in upload for images

--- router/course.js
+++ router/course.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const multiparty = require("connect-multiparty");
 const CourseController = require("../controllers/course");
 const md_auth = require("../middlewares/authenticated");
 
 const api = express.Router();
+const md_upload = multiparty({ uploadDir: "uploads/course" });
 
 api.post("/course", [md_auth.asureAuth], CourseController.createCourse);
 
