eed9b5ba79434d4e33ae2cb038b264e031dd80a8
NestorSanchez <nesaenz.69@gmail.com>
2023-03-08 11:55:51 -0500

    add function getEmail with pagination newslatter

--- controllers/newsletter.js
+++ controllers/newsletter.js
@@ -1,3 +1,35 @@
 const Newsletter = require("../models/newslettter");
 
-module.exports = {};
+function suscribeEmail(req, res) {
+  const { email } = req.body;
+  if (!email) res.status(400).send({ message: "Email obligatorio" });
+  const newsletter = new Newsletter({
+    email: email.toLowerCase(),
+  });
+
+  newsletter.save((error) => {
+    if (error) {
+      res.status(400).send({ msg: "Email registrado" });
+    } else {
+      res.status(200).send({ msg: "Email registrado con exito" });
+    }
+  });
+}
+
+function getEmails(req, res) {
+  const { page = 1, limit = 10 } = req.query;
+
+  const options = {
+    page: parseInt(page),
+    limit: parseInt(limit),
+  };
+
+  Newsletter.paginate({}, options, (error, emailStored) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al obtener los email" });
+    } else {
+      res.status(200).send(emailStored);
+    }
+  });
+}
+module.exports = { suscribeEmail, getEmails };
--- router/newsletter.js
+++ router/newsletter.js
@@ -3,5 +3,7 @@ const NewsletterController = require("../controllers/newsletter");
 const md_auth = require("../middlewares/authenticated");
 
 const api = express.Router();
+api.post("/newsletter", NewsletterController.suscribeEmail);
+api.get("/newsletter", [md_auth.asureAuth], NewsletterController.getEmails);
 
 module.exports = api;
